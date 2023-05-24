3fb73a7e2980ced8d3312e385a3ed284175377da
matteo argenio <matteargy@gmail.com>
2023-05-24 17:00:14 +0200

    doing bootstrap

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Restaurant.destroy_all
+
+Restaurant.create(name: "Epicure", category: "french", address: "madird")
+Restaurant.create(name: "mcdonald", category: "italian", address: "milan")
+Restaurant.create(name: "Taco", category: "french", address: "paris")
+Restaurant.create(name: "billy", category: "chinese", address: "china")
+Restaurant.create(name: "pizza", category: "italian", address: "rome")
+Review.create(content: "yummy yummy", rating: 4)
+Review.create(content: "bueno", rating: 5)
+Review.create(content: "mhhh", rating: 2)
+Review.create(content: "shit", rating: 1)
+Review.create(content: "blah", rating: 3)
